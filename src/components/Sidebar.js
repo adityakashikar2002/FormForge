118f88a36b2fd63da0292fe927e911bda2c10733
adityakashikar2002 <adityakashikar02@gmail.com>
2025-03-17 12:46:07 +0530

    slide bar changes done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,79 @@
+// // Sidebar.js
+// import React from 'react';
+// import { useDispatch } from 'react-redux';
+// import { addField } from '../store/formSlice';
+// import { v4 as uuidv4 } from 'uuid';
+// import { FaFont, FaHashtag, FaEnvelope, FaLock, FaParagraph, FaList, FaCheckSquare, FaDotCircle, FaCalendar, FaUpload } from 'react-icons/fa';
+// import { AiOutlinePlus} from 'react-icons/ai';
+// import './Sidebar.css';
+
+// const Sidebar = () => {
+//   const dispatch = useDispatch();
+
+//   const addNewField = (type, label, options = []) => {
+//     dispatch(addField({
+//       id: uuidv4(),
+//       label,
+//       type,
+//       required: false,
+//       options,
+//     }));
+//   };
+
+//   return (
+//     <div className="sidebar">
+//       <h3 className="sidebar-title"><AiOutlinePlus size={18} /> Add Field</h3>
+//       <button className="sidebar-btn" onClick={() => addNewField('text', 'New Text Field')}>
+//         <FaFont className="icon" /> Text Field
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('number', 'New Number Field')}>
+//         <FaHashtag className="icon" /> Number Field
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('email', 'New Email Field')}>
+//         <FaEnvelope className="icon" /> Email Field
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('password', 'New Password Field')}>
+//         <FaLock className="icon" /> Password Field
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('textarea', 'New Textarea Field')}>
+//         <FaParagraph className="icon" /> Text Area
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('dropdown', 'New Dropdown', ['Option 1', 'Option 2'])}>
+//         <FaList className="icon" /> Dropdown
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('checkbox', 'New Checkbox Group')}>
+//         <FaCheckSquare className="icon" /> Checkbox Group
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('radio', 'New Radio Group')}>
+//         <FaDotCircle className="icon" /> Radio Group
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('date', 'New Date Picker')}>
+//         <FaCalendar className="icon" /> Date Picker
+//       </button>
+//       <button className="sidebar-btn" onClick={() => addNewField('file', 'New File Upload')}>
+//         <FaUpload className="icon" /> File Upload
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
 // Sidebar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addField } from '../store/formSlice';
 import { v4 as uuidv4 } from 'uuid';
-import { FaFont, FaHashtag, FaEnvelope, FaLock, FaParagraph, FaList, FaCheckSquare, FaDotCircle, FaCalendar, FaUpload } from 'react-icons/fa';
-import { AiOutlinePlus} from 'react-icons/ai';
+import { FaBars, FaTimes, FaFont, FaHashtag, FaEnvelope, FaLock, FaParagraph, FaList, FaCheckSquare, FaDotCircle, FaCalendar, FaUpload } from 'react-icons/fa';
+import { AiOutlinePlus } from 'react-icons/ai';
 import './Sidebar.css';
 
 const Sidebar = () => {
   const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   const addNewField = (type, label, options = []) => {
     dispatch(addField({
@@ -21,39 +86,44 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="sidebar">
-      <h3 className="sidebar-title"><AiOutlinePlus size={18} /> Add Field</h3>
-      <button className="sidebar-btn" onClick={() => addNewField('text', 'New Text Field')}>
-        <FaFont className="icon" /> Text Field
+    <>
+      <button className="toggle-btn" onClick={toggleSidebar}>
+        {isOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
       </button>
-      <button className="sidebar-btn" onClick={() => addNewField('number', 'New Number Field')}>
-        <FaHashtag className="icon" /> Number Field
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('email', 'New Email Field')}>
-        <FaEnvelope className="icon" /> Email Field
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('password', 'New Password Field')}>
-        <FaLock className="icon" /> Password Field
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('textarea', 'New Textarea Field')}>
-        <FaParagraph className="icon" /> Text Area
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('dropdown', 'New Dropdown', ['Option 1', 'Option 2'])}>
-        <FaList className="icon" /> Dropdown
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('checkbox', 'New Checkbox Group')}>
-        <FaCheckSquare className="icon" /> Checkbox Group
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('radio', 'New Radio Group')}>
-        <FaDotCircle className="icon" /> Radio Group
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('date', 'New Date Picker')}>
-        <FaCalendar className="icon" /> Date Picker
-      </button>
-      <button className="sidebar-btn" onClick={() => addNewField('file', 'New File Upload')}>
-        <FaUpload className="icon" /> File Upload
-      </button>
-    </div>
+      <div className={`sidebar ${isOpen ? 'sidebar-open' : ''}`}>
+        <h3 className="sidebar-title"><AiOutlinePlus size={18} /> Add Field</h3>
+        <button className="sidebar-btn" onClick={() => addNewField('text', 'New Text Field')}>
+          <FaFont className="icon" /> Text Field
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('number', 'New Number Field')}>
+          <FaHashtag className="icon" /> Number Field
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('email', 'New Email Field')}>
+          <FaEnvelope className="icon" /> Email Field
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('password', 'New Password Field')}>
+          <FaLock className="icon" /> Password Field
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('textarea', 'New Textarea Field')}>
+          <FaParagraph className="icon" /> Text Area
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('dropdown', 'New Dropdown', ['Option 1', 'Option 2'])}>
+          <FaList className="icon" /> Dropdown
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('checkbox', 'New Checkbox Group')}>
+          <FaCheckSquare className="icon" /> Checkbox Group
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('radio', 'New Radio Group')}>
+          <FaDotCircle className="icon" /> Radio Group
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('date', 'New Date Picker')}>
+          <FaCalendar className="icon" /> Date Picker
+        </button>
+        <button className="sidebar-btn" onClick={() => addNewField('file', 'New File Upload')}>
+          <FaUpload className="icon" /> File Upload
+        </button>
+      </div>
+    </>
   );
 };
 
